f0bd0d8254062a37e3bc7e60b4222a3f65c89095
Freddy Cabrera <40347365+Fredjhonas@users.noreply.github.com>
2024-06-12 23:50:31 -0400

    feat: add query client for request

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
+import { QueryClientProvider } from '@tanstack/react-query';
 import { NativeBaseProvider } from 'native-base';
 import React, { useEffect } from 'react';
 import SplashScreen from 'react-native-splash-screen';
 
+import queryClient from './src/api/queryClient';
 import AppNavigation from './src/navigation';
 import theme from './src/theme';
 
@@ -11,9 +13,11 @@ const App = () => {
   }, []);
 
   return (
-    <NativeBaseProvider theme={theme}>
-      <AppNavigation />
-    </NativeBaseProvider>
+    <QueryClientProvider client={queryClient}>
+      <NativeBaseProvider theme={theme}>
+        <AppNavigation />
+      </NativeBaseProvider>
+    </QueryClientProvider>
   );
 };
 
